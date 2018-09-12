0e63cd613ba2a56f15d28573039ea6cd1d37606a
Amy Leigh Petrie <apetrie9849@gmail.com>
2018-09-12 13:34:00 -0600

    finished iteration 1

--- lib/vendor.rb
+++ lib/vendor.rb
@@ -1,16 +1,25 @@
+require 'pry'
 class Vendor
   attr_reader :name,
               :inventory
 
-  def initialize(name)
+  def initialize(name, inventory=Hash.new(0))
     @name = name
-    @inventory = Hash.new(0)
+    @inventory = inventory
   end
 
   def check_stock(wanted_item)
+    wanted_item = wanted_item.capitalize
     if @inventory.keys.include?(wanted_item)
-      return @inventory[wanted_item]
+      @inventory[wanted_item]
     else @inventory[wanted_item] = 0
     end
   end
+
+  def stock(item, number)
+    item = item.capitalize
+    check_stock(item)
+    @inventory[item] += number
+  end
+
 end
--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -1,3 +1,4 @@
+require 'pry'
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/vendor'
@@ -21,10 +22,34 @@ class TestVendor < Minitest::Test
     assert_equal Hash.new, vendor.inventory
   end
 
-  def check_stock(item)
+  def test_check_stock_for_new_item
     vendor = Vendor.new("Rocky Mountain Fresh")
 
-    assert_equal 0, inventory.check_stock("peaches")
+    assert_equal 0, vendor.check_stock("peaches")
   end
-  
+
+  def test_stock_adds_items_to_inventory
+    vendor = Vendor.new("Rocky Mountain Fresh")
+    vendor.stock("peaches", 30)
+
+    assert_equal 30, vendor.check_stock("peaches")
+  end
+
+  def test_stock_adds_items_to_inventory
+    vendor = Vendor.new("Rocky Mountain Fresh")
+    vendor.stock("peaches", 30)
+
+    assert_equal 30, vendor.check_stock("peaches")
+  end
+
+  def test_the_inventory_is_additive_when_stocked
+    vendor = Vendor.new("Rocky Mountain Fresh")
+    vendor.stock("Peaches", 30)
+    vendor.stock("Peaches", 25)
+    vendor.stock("Tomatoes", 12)
+    expected = {"Peaches" => 55, "Tomatoes" => 12}
+    
+    assert_equal expected, vendor.inventory
+  end
+
 end
